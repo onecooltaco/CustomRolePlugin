ffa7f162fb9359c41db630d4851daa7f6d630993
Jeremy Leggat <jleggat@asu.edu>
2020-11-23 18:10:14 -0700

    Update class-multisite-custom-role.php
    add more caps

--- includes/class-multisite-custom-role.php
+++ includes/class-multisite-custom-role.php
@@ -13,9 +13,17 @@ class Multisite_Custom_Role
             'manage_network_plugins',
             'manage_network_themes',
             'install_plugins',
+            'install_themes',
+            'update_plugins',
+            'update_themes',
+            'upload_plugins',
+            'upload_themes',
             'activate_plugins',
+            'activate_themes',
             'edit_plugins',
-            'delete_plugins'
+            'edit_themes',
+            'delete_plugins',
+            'delete_themes'
         );
 
         // add all the capabilities by looping through them
