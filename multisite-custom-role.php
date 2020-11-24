3f4c80262e2610f5344a6c3a7a0b1aabeb639484
Jeremy Leggat <jleggat@asu.edu>
2020-11-23 17:26:58 -0700

    Update multisite-custom-role.php

--- multisite-custom-role.php
+++ multisite-custom-role.php
@@ -8,7 +8,7 @@ Version:  1.0
 Description:  Customize WordPress User Roles.
 Author:  Jeremy Leggat
 Author URI:  https://cronkite.asu.edu
-GitHub Plugin URI: https://github.com/onecooltaco/CustomRolePlugin
+GitHub Plugin URI: https://github.com/onecooltaco/Multisite-Custom-Role
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  custom-role
